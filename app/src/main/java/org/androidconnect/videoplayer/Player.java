d594567873f8229d90b6f968b2aa0cfd8da0d8f0
amallad2 <amallad2@xtec.cat>
2016-12-12 13:47:48 +0100

    Add files via upload

---
+++ app/src/main/java/org/androidconnect/videoplayer/Player.java
@@ -0,0 +1,161 @@
+package org.androidconnect.videoplayer;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+
+public class Player extends Activity implements
+        OnBufferingUpdateListener, OnCompletionListener,
+        OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback {
+
+    private static final String TAG = "MediaPlayerDemo";
+    private int mVideoWidth;
+    private int mVideoHeight;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+    private String path;
+    private Bundle extras;
+    private static final String MEDIA = "media";
+    private static final String URL = "url";
+
+    private boolean mIsVideoSizeKnown = false;
+    private boolean mIsVideoReadyToBePlayed = false;
+
+    /**
+     * 
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.mediaplayer);
+        mPreview = (SurfaceView) findViewById(R.id.surface);
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        extras = getIntent().getExtras();
+        //holder.setFixedSize(500 , 400);
+
+
+    }
+
+    private void playVideo(Integer Media) {
+        doCleanUp();
+        try {
+            Log.d(TAG, "Path: " + path);
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer();
+           	mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnVideoSizeChangedListener(this);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "error: " + e.getMessage(), e);
+        }
+    }
+
+    public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+        Log.d(TAG, "onBufferingUpdate percent:" + percent);
+
+    }
+
+    public void onCompletion(MediaPlayer arg0) {
+        Log.d(TAG, "onCompletion called");
+    }
+
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        Log.v(TAG, "onVideoSizeChanged called");
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+            return;
+        }
+        mIsVideoSizeKnown = true;
+        mVideoWidth = width;
+        mVideoHeight = height ;
+        mVideoWidth = 1000;
+        mVideoHeight = 700;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.d(TAG, "onPrepared called");
+        mIsVideoReadyToBePlayed = true;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
+        path = extras.getString(URL);
+        playVideo(extras.getInt(MEDIA));
+        TextView text = (TextView) findViewById(R.id.txt);
+        text.setText(path);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void doCleanUp() {
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+        mIsVideoReadyToBePlayed = false;
+        mIsVideoSizeKnown = false;
+    }
+
+    private void startVideoPlayback() {
+        Log.v(TAG, "startVideoPlayback");
+        holder.setFixedSize(mVideoWidth, mVideoHeight);
+        mMediaPlayer.start();
+    }
+}
---
+++ app/src/main/java/org/androidconnect/videoplayer/VideoPlayerActivity.java
@@ -0,0 +1,47 @@
+package org.androidconnect.videoplayer;
+
+
+import org.androidconnect.videoplayer.R.id;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class VideoPlayerActivity extends Activity {
+	
+	private static final String MEDIA = "media";
+	private static final String URL = "url";
+
+	private TextView txt;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		txt = (TextView) findViewById(id.url);
+		txt.setText("http://provenapps.cat/chiquito.mp4");
+        Log.d("VideoPlayerActivity	", "Created");
+        
+        Button btnvideo = (Button) findViewById(R.id.btnPlayer);
+        btnvideo.setOnClickListener(new clickVideo() );
+    }
+	
+	private class clickVideo implements OnClickListener{
+
+		public void onClick(View v) {
+			Intent intent = new Intent( v.getContext() , Player.class);
+			intent.putExtra(MEDIA, 4);
+			TextView txt = (TextView) findViewById(id.url);
+			String url = (String) txt.getText().toString() ;
+			intent.putExtra	(URL, url);
+			startActivity(intent);
+		}
+		
+	}
+}
